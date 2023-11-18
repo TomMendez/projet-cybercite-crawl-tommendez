9812c25113427b3685e03e9edd0be942c5d0f936
TomMendez <tom.mendez.mailbox@gmail.com>
2023-11-18 11:53:13 -0500

    fonctionnement basique OK

--- src/Controller/CrawlController.php
+++ src/Controller/CrawlController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Crawl;
-
+use App\Entity\Client;
 use Doctrine\ORM\EntityManagerInterface;
+include '../../Client.php';
+use DateTimeInterface;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,14 +17,37 @@ class CrawlController extends AbstractController
     #[Route('/crawl', name: 'app_crawl')]
     public function index(EntityManagerInterface $entityManager): Response
     {
+        # COPIE CODE GITHUB MODELE
+        $config = json_decode(file_get_contents(__DIR__ . "/config.json"), true); # COPIE DU FICHIER PAS ELEGANT
+        $datagarden = new Client(
+            $config['user'],
+            $config['password'],
+            $config['url']);
+        $siteId = $config['site']['id'];
+
+        $datagarden->login();
+        $aSite = $datagarden->getSiteInfos($siteId);
+        $racId = $aSite['rapports'][0]['id'];
+        $racInfos = $datagarden->getRapportInfos($racId);
+        $moteurs = $racInfos['moteurs'];
+        $date = $racInfos['last_date'];
+        foreach($moteurs as $aMoteur) {
+            $params=["date" =>$date, "outId" => $aMoteur["id"], "expressionId" => "352190"];
+            $rsltPositions = $datagarden->getRapportPositionsResultats($racId, $params);
+        }
+        # COPIE CODE GITHUB MODELE
+
+
         $crawl = new Crawl();
-        $crawl->setIdSite(0);
-        $date = \DateTime::createFromFormat('Y-m-d', '2022-11-11'); #TEMPORAIRE
-        $crawl->setDate($date);
-        $crawl->setResCrawl("test");
+        $crawl->setIdSite($siteId);
+        $datetime = \DateTime::createFromFormat(DateTimeInterface::ISO8601, $date); #TEMPORAIRE
+        $crawl->setDate($datetime);
+        $rescrawl="{URLSite = {$rsltPositions[0]['url']}}"."<br>"."{Moteur = {$rsltPositions[0]['moteur']['name']}}"."<br>"."{Position = {$rsltPositions[0]['position']}}"."<br>";
+        # var_dump($rescrawl);
+        $crawl->setResCrawl($rescrawl);
 
         $entityManager->persist($crawl);
-        $entityManager->flush(); # PROBLEME CETTE LIGNE CAUSE UNE ERREUR
+        $entityManager->flush();
         return $this->render('crawl/index.html.twig', [
             'controller_name' => 'CrawlController',
         ]);
---
+++ src/Entity/Client.php
@@ -0,0 +1,209 @@
+<?php
+namespace App\Entity;
+
+use GuzzleHttp\Client as GuzzleClient;
+
+class Client {
+
+    protected $_token = "";
+    protected $_username = "";
+    protected $_password = "";
+    protected $_baseUrl = "";
+
+    /**
+     * Représente le curl qui se connecte à l'api
+     * @var GuzzleClient
+     */
+    protected $_connection;
+
+    public const GET = 'GET';
+    public const POST = 'POST';
+    public const PUT = 'PUT';
+    public const DELETE = 'DELETE';
+    public const PATCH = 'PATCH';
+
+    public function __construct($username, $password, $baseurl) {
+        $this->_username = $username;
+        $this->_password = $password;
+        $this->_baseUrl = $baseurl;
+        $this->_connection = new GuzzleClient(['base_uri' => $baseurl]);
+    }
+
+    /**
+     * Retourne le token d'authentification de la session
+     * @return string
+     */
+    public function getToken():string
+    {
+        return $this->_token;
+    }
+
+    /**
+     * Authentifie l'utilisateur et retourne le token d'authentification
+     * @return string
+     */
+    public function login():string
+    {
+        $params = [
+            'json' => [
+                'username' => $this->_username,
+                'password' => $this->_password,
+                ]
+            ];
+        $response = $this->execute(self::POST, "login", $params);
+        $this->_token = $response['token'];
+        return $this->_token;
+    }
+
+    /**
+     * Recupère les informations sur le site passé en paramètre
+     * @var string $idSite
+     * @return array
+    */
+    public function getSiteInfos(string $idSite):array
+    {
+        $uri = sprintf("sites/%s", $idSite);
+        $params = [
+            "headers" => [
+                'Authorization' => 'Bearer ' . $this->_token,
+                'Accept'        => 'application/json',
+            ]
+        ];
+        return $this->execute(self::GET, $uri, $params);
+    }
+
+    /**
+     * Retourne les informations sur le rapport passé en paramètre
+     *
+     * @param string $idRapport
+     * @return array
+     */
+    public function getRapportInfos(string $idRapport):array
+    {
+        $uri = sprintf("rapports/%s", $idRapport);
+
+        $params = [
+            "headers" => [
+                'Authorization' => 'Bearer ' . $this->_token,
+                'Accept'        => 'application/json',
+            ]
+        ];
+        return $this->execute(self::GET, $uri, $params);
+    }
+
+    /**
+     * Retourne les résultats de positions pour le rapport passé en paramètre
+     * Paramètres obligatoires :
+     *  "date" => (string) format : AAAA-MM-JJ la date du rapport auquel on veut les résultats
+     *  "outId" => (int/string) l'id de l'outil sur lequel on veut des résultats
+     * Paramètres disponibles :
+     *  "expressionId" => (int|string) l'id de l'expression
+     *  "pagination" => "true|false" (string)
+     *  "itemsPerPage" => (int|string) nombre d'items à afficher par page
+     *  "page" => (int|string) page demandée
+     *
+     * @param string $idRapport
+     * @param array|null $apiParams
+     * @return array
+     */
+    public function getRapportPositionsResultats(string $idRapport, ?array $apiParams = null):array
+    {
+        $uri = sprintf("rapports/%s/positions/resultats/", $idRapport);
+
+        if ($apiParams !== null) {
+            $uri.="?".http_build_query($apiParams,"","&",);
+        }
+
+        $params = [
+            "headers" => [
+                'Authorization' => 'Bearer ' . $this->_token,
+                'Accept'        => 'application/json',
+            ]
+        ];
+        return $this->execute(self::GET, $uri, $params);
+    }
+
+    /**
+     * Retourne les dates du rapport dont l'id est passé en paramètre
+     * Paramètres disponibles :
+     *  "pagination" => "true|false" (string)
+     *  "itemsPerPage" => (int|string) nombre d'items à afficher par page
+     *  "page" => (int|string) page demandée
+     *
+     * @param string $idRapport
+     * @param array|null $apiParams
+     * @return array
+     */
+    public function getRapportDates(string $idRapport, ?array $apiParams = null): array
+    {
+        $uri = sprintf("rapports_dates?racId=%s", $idRapport);
+
+        if ($apiParams !== null) {
+            $uri.="&".http_build_query($apiParams,"","&",);
+        }
+
+        $params = [
+            "headers" => [
+                'Authorization' => 'Bearer ' . $this->_token,
+                'Accept'        => 'application/json',
+            ]
+        ];
+        return $this->execute(self::GET, $uri, $params);
+    }
+
+    /**
+     * Retourne toutes les expressions liées à un rapport.
+     * Paramètres disponibles :
+     *  "pagination" => "true|false" (string)
+     *  "itemsPerPage" => (int|string) nombre d'items à afficher par page
+     *  "page" => (int|string) page demandée
+     *
+     * @param string $idRapport
+     * @param array|null $apiParams
+     * @return array
+     */
+    public function getRapportExpressionsLiees(string $idRapport, ?array $apiParams = null):array
+    {
+        $uri = "rapports/expressions";
+
+        $apiParams['racId'] = $idRapport;
+
+        if ($apiParams !== null) {
+            $uri.="?".http_build_query($apiParams,"","&",);
+        }
+
+        $params = [
+            "headers" => [
+                'Authorization' => 'Bearer ' . $this->_token,
+                'Accept'        => 'application/json',
+            ]
+        ];
+        return $this->execute(self::GET, $uri, $params);
+    }
+
+    /**
+     * Effectue un appel vers l'url $url en utilisant la méthode $method en passant les paramètres $params
+     * Retourne le résultat de l'appel sous la forme d'un tableau
+     * @param string $method Il est préférable d'utiliser les constantes de la classe client
+     * @param string $url Uri relative à l'url de base de l'api. Une url absolue peut également être utilisée
+     * @param array $params
+     * @return array
+     */
+    public function execute($method=self::GET, string $url, ?array  $params=null):array
+    {
+        try {
+            $response = $this->_connection->$method($url, $params);
+            return json_decode($response->getBody()->getContents(), true);
+        }catch (\Exception $exp) {
+            throw new Exception(
+                $exp->getMessage(),
+                $url,
+                $method,
+                sprintf("Erreur %s lors de l'appel de %s",
+                    $exp->getCode(),
+                    $url),
+                $exp->getCode()
+            );
+        }
+    }
+}
