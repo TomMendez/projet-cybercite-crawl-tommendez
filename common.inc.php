f3aec499c310cf52c827b28514baa556dc1df322
TomMendez <tom.mendez.mailbox@gmail.com>
2023-11-21 09:30:55 -0500

    debug (apres rendu oubli)

--- common.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'Client.php';
-
-    $config = json_decode(file_get_contents("config.json"), true);
-    $datagarden = new Client(
-        $config['user'],
-        $config['password'],
-        $config['url']);
-    $siteId = $config['site']['id'];
